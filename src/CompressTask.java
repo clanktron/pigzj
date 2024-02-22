59653cefd2647404744801f62ad76e75e5df1436
clayton <crdcastro5@gmail.com>
2024-02-21 21:10:04 -0800

    ensure right options are set for deflator

--- src/CompressTask.java
+++ src/CompressTask.java
@@ -4,15 +4,18 @@ import java.util.concurrent.*;
 class CompressTask implements Runnable {
     private static final int BLOCK_SIZE = 128 * 1024;
     private final ProcessQueue processQueue; 
+    private final byte[] dictionary;
     private final byte[] outputBuffer;
 
     public CompressTask(ProcessQueue processQueue) {
         this.processQueue = processQueue;
+        this.dictionary = new byte['\u8000'];
         this.outputBuffer = new byte[BLOCK_SIZE];
     }
 
     public void run() {
-        Deflater deflater = new Deflater();
+        Deflater deflater = new Deflater(-1, true);
+        deflater.setDictionary(this.dictionary);
         Block block = this.processQueue.getNextUncompressedBlock();
         deflater.setInput(block.content);
         deflater.finish();
